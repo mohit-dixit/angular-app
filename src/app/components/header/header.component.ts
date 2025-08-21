ed502c18db01335980c2f1c96250c2ce94d6a692
unknown <mohit.dixit354@gmail.com>
2025-08-21 11:33:28 +0530

    changed the session timer position from sidemenu to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,8 +4,16 @@ import { CommonService } from '../../services/common/common.service';
 import { MatToolbar } from '@angular/material/toolbar';
 import { MatIcon } from '@angular/material/icon';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import {MatRippleModule} from '@angular/material/core';
+import { Router, RouterLink } from '@angular/router';
+import { MatRippleModule } from '@angular/material/core';
+import { Observable } from 'rxjs';
+import { TimerService } from '../../services/timer/timer.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ModalComponent } from '../modal/modal.component';
+import { ModalTitle, ModalTypes } from '../../constants/globalconst';
+import { environment } from '../../environments/environment';
+import { HttpService } from '../../services/http/http.service';
+import { SnackbarService } from '../../services/snackbar/snackbar.service';
 @Component({
   selector: 'app-header',
   imports: [
@@ -20,18 +28,19 @@ import {MatRippleModule} from '@angular/material/core';
 })
 export class HeaderComponent {
 
-  centered = false;
-  disabled = false;
-  unbounded = false;
-
-  radius = 50;
-  color = 'black';
-
+  time$!: Observable<number>;
 
   constructor(
+    private _router: Router,
+    private _service: HttpService,
     private _tokenService: TokenManagerService,
-    private _commonService: CommonService
-  ) {}
+    private _commonService: CommonService,
+    private _timerService: TimerService,
+    private _dialog: MatDialog,
+    private _snackbar: SnackbarService,
+  ) {
+    this.time$ = this._timerService.time$;
+  }
 
   hamburgerClick() {
     this._commonService.hamburgerClick();
@@ -51,5 +60,59 @@ export class HeaderComponent {
 
   logOut() {
     this._commonService.logOutClick();
+  }
+
+  formatTime(seconds: number): string {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    if (mins === 0 && secs === 1) {
+      this._dialog.closeAll();
+      const msg = "Your session has expired. Please log in again.";
+      this.openSessionExpiredDialog(msg, false);
+      this._router.navigate(['login']);
+    }
+    else if (this._dialog.openDialogs.length === 0) {
+      if (mins === 0 && secs === 59) {
+        const msg = "Your session is going to Expire. Do you want to extend it?";
+        this.openSessionExpiredDialog(msg, true);
+      }
+    }
+    return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
+  }
+
+  openSessionExpiredDialog(msg: string, toShowExtendButton: boolean): void {
+    this._dialog.open(ModalComponent, {
+      width: '750px',
+      closeOnNavigation: true,
+      disableClose: true,
+      data: {
+        message: msg,
+        toShowExtendButton: toShowExtendButton,
+        modaltitle: ModalTitle.SESSION_EXIPRY,
+        modaltype: ModalTypes.SESSION
+      }
+    }).afterClosed().subscribe(result => {
+      if (result) {
+        this.extendToken();
+      }
+    });
+  }
+
+  extendToken() {
+    let api = environment.apis.extend;
+    this._service.login(api, null).subscribe((data: any) => {
+      if (data && data.success) {
+        this._tokenService.setLoginUserName(data.username);
+        this._snackbar.showSuccessMessage("Session Extended Successfully");
+        this._timerService.resetTimer();
+        this._timerService.startTimer();
+      }
+      else if (data && !data.success && data.message) {
+        this._snackbar.showErrorMessage(data.message);
+      }
+    }, error => {
+      this._snackbar.showErrorMessage("Error in retrieving data. Please check the logs.");
+      console.log(error);
+    });
   }
 }
--- src/app/components/sidepanel/sidepanel.component.ts
+++ src/app/components/sidepanel/sidepanel.component.ts
@@ -3,14 +3,9 @@ import { Component, effect, ViewChild } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatListItem, MatNavList } from '@angular/material/list';
 import { MatSidenav, MatSidenavContainer } from '@angular/material/sidenav';
-import { MatToolbar } from '@angular/material/toolbar';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-import { SnackbarService } from '../../services/snackbar/snackbar.service';
 import { DeviceDetectorService } from '../../services/devicedetector/device-detector.service';
 import { environment } from '../../environments/environment';
-import { HttpService } from '../../services/http/http.service';
-import { TimerService } from '../../services/timer/timer.service';
-import { Observable } from 'rxjs';
 import { ModalComponent } from '../modal/modal.component';
 import { MatDialog } from '@angular/material/dialog';
 import { TokenManagerService } from '../../services/tokenmanager/token-manager.service';
@@ -33,23 +28,18 @@ import { CommonService } from '../../services/common/common.service';
 })
 export class SidepanelComponent {
 
-  @ViewChild('sidenav') sidenav!: MatSidenav;
-  showSubmenu: boolean = true;
-  time$!: Observable<number>;
+  @ViewChild('sidenav') sidenav!: MatSidenav;  
   private context: any;
-  myMenuExpanded = false;
+  showNavigationMenu: boolean = true;
+  showAdminSubmenu: boolean = true;
 
   constructor(private _router: Router,
-    private _snackbar: SnackbarService,
     private _devicedetector: DeviceDetectorService,
-    private _service: HttpService,
-    private _timerService: TimerService,
     private _dialog: MatDialog,
     private _tokenService: TokenManagerService,
     private _configService: AppConfigService,
     private _commonService: CommonService
-  ) {
-    this.time$ = this._timerService.time$;
+  ) {   
     this.context = this._configService.getConfig().context;
   }
 
@@ -76,43 +66,9 @@ export class SidepanelComponent {
     if (isMobile) {
       this.hamburgerClick();
     }
-  }
+  }  
 
-  formatTime(seconds: number): string {
-    const mins = Math.floor(seconds / 60);
-    const secs = seconds % 60;
-    if (mins === 0 && secs === 1) {
-      this._dialog.closeAll();
-      const msg = "Your session has expired. Please log in again.";
-      this.openSessionExpiredDialog(msg, false);
-      this._router.navigate(['login']);
-    }
-    else if (this._dialog.openDialogs.length === 0) {
-      if (mins === 0 && secs === 59) {
-        const msg = "Your session is going to Expire. Do you want to extend it?";
-        this.openSessionExpiredDialog(msg, true);
-      }
-    }
-    return `${mins}:${secs < 10 ? '0' : ''}${secs}`;
-  }
-
-  openSessionExpiredDialog(msg: string, toShowExtendButton: boolean): void {
-    this._dialog.open(ModalComponent, {
-      width: '750px',
-      closeOnNavigation: true,
-      disableClose: true,
-      data: {
-        message: msg,
-        toShowExtendButton: toShowExtendButton,
-        modaltitle: ModalTitle.SESSION_EXIPRY,
-        modaltype: ModalTypes.SESSION
-      }
-    }).afterClosed().subscribe(result => {
-      if (result) {
-        this.extendToken();
-      }
-    });
-  }
+  
 
   showLogOutModalPopup(msg: string, toShowExtendButton: boolean): void {
     this._dialog.open(ModalComponent, {
@@ -139,29 +95,7 @@ export class SidepanelComponent {
 
   swagger() {
     window.open(this.context + environment.apis.swagger, '_blank');
-  }
-
-  extendToken() {
-    let api = environment.apis.extend;
-    this._service.login(api, null).subscribe((data: any) => {
-      if (data && data.success) {
-        this._tokenService.setLoginUserName(data.username);
-        this._snackbar.showSuccessMessage("Session Extended Successfully");
-        this._timerService.resetTimer();
-        this._timerService.startTimer();
-      }
-      else if (data && !data.success && data.message) {
-        this._snackbar.showErrorMessage(data.message);
-      }
-    }, error => {
-      this._snackbar.showErrorMessage("Error in retrieving data. Please check the logs.");
-      console.log(error);
-    });
-  }
-
-  toggleMyMenu() {
-    this.myMenuExpanded = !this.myMenuExpanded;
-  }
+  }  
 
   private hamburgerClickEffect = effect(() => {
     const value = this._commonService.triggerhamburgerClickSignal();
@@ -188,5 +122,32 @@ export class SidepanelComponent {
     this.hamburgerClickEffect.destroy();
     this.hideOnMobileClickEffect.destroy();
     this.logOutClickEffect.destroy();
+  }
+
+  clickNavigationParent(){
+    this.showNavigationMenu = !this.showNavigationMenu
+    const submenu = document.getElementById('navigationsubmenu');
+    let style = !this.showNavigationMenu ? 'none' : 'block';  
+    this.changeStyleOfSubmenu(submenu, style);
+  }
+
+  clickAdminNavigationParent(){
+    this.showAdminSubmenu = !this.showAdminSubmenu
+    const submenu = document.getElementById('adminnavigationsubmenu');
+    let style = !this.showAdminSubmenu ? 'none' : 'block';  
+    this.changeStyleOfSubmenu(submenu, style);
+  }
+
+  changeStyleOfSubmenu(submenu: HTMLElement | null, style: string) {
+    if (submenu) {
+      if (style === 'none') {
+        setTimeout(() => {
+          submenu.style.display = style;
+        }, 300);
+      }
+      else {
+        submenu.style.display = style;
+      }
+    }
   }
 }
